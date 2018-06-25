4c653b237a5669214c7ad9e1731aa27560332180
angel <angelo12@vt.edu>
2018-06-25 13:17:45 +0200

    Added SDL and library support on CMAKE.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,27 @@
 #include <stdio.h>
-int main(){
-    printf("Hello World, You have now been changed!\n");
+#include "SDL.h"
+
+int main(int argc, char *argv[]){
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL2Test",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        640,
+        480,
+        0
+    );
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    SDL_SetRenderDrawColor(renderer, 0,0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
